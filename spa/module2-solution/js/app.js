6a17f3e09eca24eca4112cb433735229261b06bc
Georgios Karametas <geometas@hotmail.com>
2016-09-24 16:10:14 +0300

    Added first list

--- spa/module2-solution/js/app.js
+++ spa/module2-solution/js/app.js
@@ -6,16 +6,31 @@
 	.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
 	.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
+	ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
 	function ToBuyShoppingController(ShoppingListCheckOffService) {
 		var toBuyList = this;
+
+		toBuyList.items = ShoppingListCheckOffService.items;
 	}
 
+	AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
 	function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
 		var alreadyBoughtList = this;
 	}
 
 	function ShoppingListCheckOffService() {
+		var service = this;
 		
+		service.items = [
+			{ name: '10 cookies' },
+			{ name: '1 bicycle' },
+			{ name: '10 tomatoes' },
+			{ name: '100 lollipops' }
+		];
+
+		service.itemBought = function (index) {
+			console.log(index);
+		};
 	}
 
 })()
