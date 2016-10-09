7ea5c3a34e692acf7e3ff89224735e9d863f0fa0
Georgios Karametas <geometas@hotmail.com>
2016-10-09 23:49:00 +0300

    Changes

--- spa/module4-solution/src/data/items.controller.js
+++ spa/module4-solution/src/data/items.controller.js
@@ -4,11 +4,10 @@
 angular.module('data')
 .controller('CategoryItemsController', CategoryItemsController);
 
-// 'item' is injected through state's resolve
+// 'items' is injected through state's resolve
 CategoryItemsController.$inject = ['items']
 function CategoryItemsController(items) {
   var categoryItems = this;
-  console.log(items)
   categoryItems.category = items.category.name;
   categoryItems.menuItems = items.menu_items;
 }
